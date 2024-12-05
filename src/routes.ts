f500e4066977ab2dcfed7d7e052f3f2bfb1573c8
WaldomiroBarbosa <devwaldomirobarbosa@gmail.com>
2024-12-05 16:46:04 -0300

    feat: get purchase history added

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import { CatalogController } from "./models/catalog/catalog.controller";
 import { CartController } from "./models/cart/cart.controller";
+import { HistoryController } from "./models/history/history.controller";
 
 const router = Router();
 
 const catalogController = new CatalogController();
 const cartService = new CartController();
+const historyController = new HistoryController();
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
@@ -21,5 +23,7 @@ router.get("/cart/:userId", cartService.getCartItems);
 router.post("/cart/:userId", cartService.addCartItem);
 router.delete("/cart/:userId/:productId", cartService.removeCartItem);
 router.delete("/cart/:userId/clear", cartService.clearCart);
+
+router.get("/history/:userId", historyController.getCatalog);
 
 export default router;
